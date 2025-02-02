464a4a24295890387bb731e8852a6ca0e007a2d8
David Valdez Gramajo <86501090+Davidvg29@users.noreply.github.com>
2025-02-02 01:49:37 -0300

    adding files unfinished

--- app/api/createProduct/route.js
+++ app/api/createProduct/route.js
@@ -1,20 +1,59 @@
-export async function POST(request) {
-    try {
-      const { origin } = new URL(request.url); 
-      const data = await request.json()
-      const response = await fetch(`${origin}/api/getProducts`)
-      const products = await response.json()
+import multer from 'multer';
+import { promises as fs } from 'fs';
+import path from 'path';
+import { NextResponse } from 'next/server';
+import { v2 as cloudinary } from 'cloudinary';
 
-      const productExists = products.some(produc => produc.name === data.name)
-      if(productExists){
-        return Response.json({message: "Producto ya existe con este nombre, intente con otro"})
-      }else{
-         
+export async function POST(request) {
+   cloudinary.config({
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+    });
+    
+    try {
+        let data = await request.json();
+
+      if(data.name === "" || data.description === "" || data.priceList === "" || data.pricePublic ===""){
+         return NextResponse.json({ message: "Ningun campo de texto puede estar vacio" }, { status: 400 });
       }
 
-       return Response.json(products, { status: 200 })
+        // Leer productos actuales y manejar JSON vacío
+        const filePath = path.join(process.cwd(), 'data', 'products.json');
+        let products = [];
+        try {
+            const fileContents = await fs.readFile(filePath, 'utf-8');
+            products = fileContents ? JSON.parse(fileContents) : [];
+        } catch (error) {
+            if (error.code !== 'ENOENT') throw error; // Si el archivo no existe, se inicializa con un array vacío
+        }
+
+        // Verificar si el producto ya existe
+        const productExists = products.some(product => product.name === data.name);
+        if (productExists) {
+            return NextResponse.json({ message: "Producto ya existe con este nombre, intente con otro" }, { status: 400 });
+        }
+
+        try {
+         // Upload an image
+         const imagePath = path.join(process.cwd(), 'public', 'imagen.jpg');
+         const uploadResult  = await cloudinary.uploader.upload(imagePath, {public_id: data.name,})
+         if(uploadResult){
+            data = {...data, image: uploadResult.secure_url}
+         }
+        } catch (error) {
+         console.log('Error al subir la imagen:', error);
+         return NextResponse.json({ message: 'Ocurrió un error al subir la imagen, producto no creado' }, { status: 500 });
+        }
+
+        // Agregar el nuevo producto y escribir en el archivo JSON
+        const updatedProducts = [...products, data];
+        await fs.writeFile(filePath, JSON.stringify(updatedProducts, null, 2), 'utf-8');
+
+        return NextResponse.json({ message: "Producto agregado correctamente" }, { status: 200 });
+
     } catch (error) {
-       return Response.json({ error: "Error al procesar la solicitud "+error.message }, { status: 500 });
+      // console.log(error)
+      return NextResponse.json({ message: "Error al crear un nuevo producto: " + error.message }, { status: 500 });
     }
- }
- 
+}
--- components/Products.js
+++ components/Products.js
@@ -22,11 +22,18 @@ import { Label } from './ui/label';
 export default function Products() {
     const [products, setProducts] = useState([])
     const [productsSearch, setProductsSearch] = useState("")
-    const filteredProducts = products.filter((product) =>
-        product.name.toLowerCase().includes(productsSearch.toLowerCase()) || 
-        product.description.toLowerCase().includes(productsSearch.toLowerCase())
-      );
-
+    const filteredProducts = products.length > 0 && products.filter((product) =>
+      (product.name && product.name.toLowerCase().includes(productsSearch.toLowerCase())) || 
+      (product.description && product.description.toLowerCase().includes(productsSearch.toLowerCase()))
+    );
+    const [data, setData] = useState({
+      name: "",
+      image: "",
+      description: "",
+      priceList: "",
+      pricePublic: ""
+    })
+    const [message, setMessage] = useState("")
     const getProducts = async () => {
         try {
             fetch('/api/getProducts')
@@ -40,8 +47,32 @@ export default function Products() {
         }
     }
 
-      useEffect(() => {getProducts()}, []);
+    useEffect(() => {getProducts()}, []);
 
+    const handleData = (e)=>{
+      setData({...data, [e.target.name]: e.target.value})
+    }
+    const sendData = async(e)=>{
+      e.preventDefault()
+      try {
+        const response = await fetch("/api/createProduct", {
+          method: "POST",
+          headers: {"Content-type": "application/json",},
+          body: JSON.stringify(data)
+        })
+        const result = await response.json()
+        if(result){
+          setMessage(result.message)
+        }else {
+          console.error('Error al crear el producto:', result.message || result.error);
+          setMessage(error)
+        }
+      } catch (error) {
+        console.log(error)
+        setMessage(error.message)
+      }
+    }
+    
   return (
     <>
       <div className=' w-full flex flex-wrap justify-center sm:justify-end items-center'>
@@ -55,26 +86,27 @@ export default function Products() {
                   {/* This action cannot be undone. This will permanently delete your account
                   and remove your data from our servers. */}
                   <Label>Nombre del producto</Label>
-                  <Input className="mb-5" placeholder="Nombre producto"/>
+                  <Input className="mb-5" placeholder="Nombre producto" name="name" value={data.name} onChange={handleData}/>
                   <Label>Cargar imagen</Label>
-                  <Input className="mb-5" id="picture" type="file" />
+                  <Input className="mb-5" id="picture" type="file" name="image" value={data.image} onChange={handleData}/>
                   <Label>Descripcion del producto</Label>
-                  <Input className="mb-5" placeholder="Descripcion producto"/>
-                  <div className='flex justify-between'>
-                    <div className='mx-1 sm:mx-0'>
+                  <Input className="mb-5" placeholder="Descripcion producto" name="description" value={data.description} onChange={handleData}/>
+                  <label className='flex justify-between'>
+                    <label className='mx-1 sm:mx-0'>
                       <Label>Precio de lista</Label>
-                      <Input className="mb-5" placeholder="Precio de lista del producto" type="number"/>
-                    </div>
-                    <div className='mx-1 sm:mx-0'>
+                      <Input className="mb-5" placeholder="Precio de lista del producto" type="number" name="priceList" value={data.priceList} onChange={handleData}/>
+                    </label>
+                    <label className='mx-1 sm:mx-0'>
                       <Label>Precio al publico</Label>
-                      <Input className="mb-5" placeholder="Precio al publico producto" type="number"/>
-                    </div>
-                  </div>
+                      <Input className="mb-5" placeholder="Precio al publico producto" type="number" name="pricePublic" value={data.pricePublic} onChange={handleData}/>
+                    </label>
+                  </label>
                 </AlertDialogDescription>
               </AlertDialogHeader>
+                <label>{message}</label>
               <AlertDialogFooter>
-                <AlertDialogCancel>Cancel</AlertDialogCancel>
-                <AlertDialogAction>Continue</AlertDialogAction>
+                <AlertDialogCancel>Cancelar</AlertDialogCancel>
+                <AlertDialogAction onClick={sendData}>Crear</AlertDialogAction>
               </AlertDialogFooter>
             </AlertDialogContent>
           </AlertDialog>
