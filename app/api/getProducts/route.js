5a9e9731d692f94fc84eb442e8428f9cd4fcb119
David Valdez Gramajo <86501090+Davidvg29@users.noreply.github.com>
2025-02-12 17:17:27 -0300

    product get of cloudinary file

--- app/api/getProducts/route.js
+++ app/api/getProducts/route.js
@@ -3,10 +3,15 @@ import path from 'path';
 
 export async function GET(request){
     try {
-        const filePath = path.join(process.cwd(), 'data', 'products.json');
-        const fileContents = await fs.readFile(filePath, 'utf-8');
-        const products = JSON.parse(fileContents);
-        return Response.json(products, {status: 200});
+        // esto trae productos de directorio
+        // const filePath = path.join(process.cwd(), 'data', 'products.json');
+        // const fileContents = await fs.readFile(filePath, 'utf-8');
+        // const products = JSON.parse(fileContents);
+
+        // lo de abajo trae el archivo de cloudinary
+        const response = await fetch("https://res.cloudinary.com/djkld6wmk/raw/upload/v1/sistema%20cosmeticos/data/nbycn2va6xg87wcos3cj.json")
+        const data = await response.json()
+        return Response.json(data, {status: 200});
     } catch (error) {
         return Response.json({ error: "Error en el servidor al obtener productos " + error.message }, { status: 500 });
     }
