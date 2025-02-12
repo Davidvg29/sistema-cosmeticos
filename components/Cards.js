d4dc5e396affade7cce04b995f838c97466ff238
David Valdez Gramajo <86501090+Davidvg29@users.noreply.github.com>
2025-02-12 17:17:44 -0300

    fixing height

--- components/Cards.js
+++ components/Cards.js
@@ -12,17 +12,17 @@ export default function Cards({product}){
     return(
         <>
             <div className="w-64 m-2">
-                <Card className="h-86">
+                <Card className="h-auto">
                     <CardHeader>
                         <CardTitle>{product.name}</CardTitle>
                         <CardDescription>{product.description}</CardDescription>
                     </CardHeader>
                     <CardContent>
-                        <img src={product.image} alt={product.name} className="w-full h-40 object-cover"/>
+                        <img src={product.image} alt={product.name} className="w-full h-64 object-contain"/>
                     </CardContent>
                     <CardFooter className="flex justify-between">
-                        <p>Lista ${product.priceList}</p>
-                        <p>Público ${product.pricePublic}</p>
+                        <p>Lista: ${product.priceList}</p>
+                        <p>Público: ${product.pricePublic}</p>
                     </CardFooter>
                 </Card>
             </div>
