189f817f993b85575c98e504cb974bbba436f529
David Valdez Gramajo <86501090+Davidvg29@users.noreply.github.com>
2025-02-13 20:42:23 -0300

    agregar funcionalidad para eliminar productos con confirmación en el componente Cards

--- components/Cards.js
+++ components/Cards.js
@@ -6,15 +6,70 @@ import {
     CardHeader,
     CardTitle,
   } from "@/components/ui/card"
-  
+  import { Trash2 } from 'lucide-react';
+  import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+  } from "@/components/ui/alert-dialog"
+import { useState } from "react";
 
-export default function Cards({product}){
+export default function Cards({product, refreshProducts}){
+    const [message, setMessage] = useState("")
+
+    const actionDelete = async()=>{
+        try {
+            const response = await fetch("/api/deleteProduct",{
+                method: "POST",
+                headers: {"Content-type": "application/json",},
+                body: JSON.stringify({ name: product.name })
+            })
+            const data = await response.json()
+            // console.log(data)
+            setMessage(data.message)
+            if(data){
+                refreshProducts()
+            }
+        } catch (error) {
+            // console.log(error)
+            setMessage("Ocurrio un error en el servidor al eliminar el producto" + error.message)
+        }
+    }
+
     return(
         <>
             <div className="w-64 m-2">
                 <Card className="h-auto">
                     <CardHeader>
-                        <CardTitle>{product.name}</CardTitle>
+                        <div className="flex justify-between">
+                            <CardTitle>{product.name}</CardTitle>
+                            {/* <Trash2 className="cursor-pointer"/> */}
+                            <AlertDialog>
+                                <AlertDialogTrigger asChild>
+                                    {/* <Button variant="outline">Show Dialog</Button> */}
+                                    <Trash2 className="cursor-pointer"/>
+                                </AlertDialogTrigger>
+                                <AlertDialogContent>
+                                    <AlertDialogHeader>
+                                    <AlertDialogTitle>Eliminar el producto?</AlertDialogTitle>
+                                    <AlertDialogDescription>
+                                        Vas a eliminar el producto con el nombre: {product.name}
+                                    </AlertDialogDescription>
+                                    </AlertDialogHeader>
+                                    <AlertDialogFooter>
+                                    <AlertDialogCancel>Cancelar</AlertDialogCancel>
+                                    <AlertDialogAction onClick={actionDelete}>Acceptar</AlertDialogAction>
+                                    </AlertDialogFooter>
+                                    <p>{message}</p>
+                                </AlertDialogContent>
+                            </AlertDialog>
+                        </div>
                         <CardDescription>{product.description}</CardDescription>
                     </CardHeader>
                     <CardContent>
@@ -26,6 +81,25 @@ export default function Cards({product}){
                     </CardFooter>
                 </Card>
             </div>
+
+            <AlertDialog>
+                <AlertDialogTrigger asChild>
+                    {/* <Button variant="outline">Show Dialog</Button> */}
+                </AlertDialogTrigger>
+                <AlertDialogContent>
+                    <AlertDialogHeader>
+                    <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                    <AlertDialogDescription>
+                        This action cannot be undone. This will permanently delete your
+                        account and remove your data from our servers.
+                    </AlertDialogDescription>
+                    </AlertDialogHeader>
+                    <AlertDialogFooter>
+                    <AlertDialogCancel>Cancel</AlertDialogCancel>
+                    <AlertDialogAction>Continue</AlertDialogAction>
+                    </AlertDialogFooter>
+                </AlertDialogContent>
+            </AlertDialog>
         </>
     )
 }
